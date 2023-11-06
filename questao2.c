431757d68bdbb62a9d3266964b21a8455e06ea14
Pedro Santos <124071877+PedroHenrique004@users.noreply.github.com>
2023-11-06 09:41:43 -0300

    Update questao2.c
    Questao 2 perfeita

--- questao2.c
+++ questao2.c
@@ -30,53 +30,48 @@ int calcularForca(struct Jogador time[11]) {
     return forcaTotal;
 }
 
-void imprimirForcaDoTime(char nomeTime[31], int forcaTotal) {
+void imprimirForcaDoTime(char nomeDoTime[31], int forcaTotal) {
     double forcaDoTime = (double)forcaTotal / 100;
-    printf("%s: %.2lf de forca\n", nomeTime, forcaDoTime);
-}
-
-char* verificarTimeMaisForte(struct Jogador time1[11], struct Jogador time2[11], char nomeTime1[31], char nomeTime2[31]) {
-    int forcaTotalTime1 = calcularForca(time1);
-    int forcaTotalTime2 = calcularForca(time2);
-
-    if (forcaTotalTime1 > forcaTotalTime2) {
-        return nomeTime1;
-    } else if (forcaTotalTime2 > forcaTotalTime1) {
-        return nomeTime2;
-    } else {
-        return "Os times tem a mesma força";
-    }
+    printf("%s: %.2lf de forca\n", nomeDoTime, forcaDoTime);
 }
 
 int main() {
     setlocale(LC_ALL, "C");
 
-    char nomeTime1[31];
-    char nomeTime2[31];
+    char nomeDoTime1[31];
+    char nomeDoTime2[31];
     struct Jogador time1[11];
     struct Jogador time2[11];
 
-    fgets(nomeTime1, sizeof(nomeTime1), stdin);
-    nomeTime1[strcspn(nomeTime1, "\n")] = '\0';
+    fgets(nomeDoTime1, sizeof(nomeDoTime1), stdin);
+    nomeDoTime1[strcspn(nomeDoTime1, "\n")] = '\0';
 
     for (int i = 0; i < 11; i++) {
         scanf("%30[^;]; %c; %d", time1[i].nome, &time1[i].posicao, &time1[i].forca);
         getchar();
     }
 
-    fgets(nomeTime2, sizeof(nomeTime2), stdin);
-    nomeTime2[strcspn(nomeTime2, "\n")] = '\0';
+    fgets(nomeDoTime2, sizeof(nomeDoTime2), stdin);
+    nomeDoTime2[strcspn(nomeDoTime2, "\n")] = '\0';
 
     for (int i = 0; i < 11; i++) {
         scanf("%30[^;]; %c; %d", time2[i].nome, &time2[i].posicao, &time2[i].forca);
         getchar();
     }
 
-    imprimirForcaDoTime(nomeTime1, calcularForca(time1));
-    imprimirForcaDoTime(nomeTime2, calcularForca(time2));
+    int forcaTotalTime1 = calcularForca(time1);
+    int forcaTotalTime2 = calcularForca(time2);
 
-    char* timeMaisForte = verificarTimeMaisForte(time1, time2, nomeTime1, nomeTime2);
-    printf("%s eh mais forte\n", timeMaisForte);
+    imprimirForcaDoTime(nomeDoTime1, forcaTotalTime1);
+    imprimirForcaDoTime(nomeDoTime2, forcaTotalTime2);
+
+    if (forcaTotalTime1 > forcaTotalTime2) {
+        printf("%s eh mais forte\n", nomeDoTime1);
+    } else if (forcaTotalTime2 > forcaTotalTime1) {
+        printf("%s eh mais forte\n", nomeDoTime2);
+    } else {
+        printf("Os times tem a mesma força\n");
+    }
 
     return 0;
 }
