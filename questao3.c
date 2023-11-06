057ddb7ed178de458972d24862aa72dd55dec399
Pedro Santos <124071877+PedroHenrique004@users.noreply.github.com>
2023-11-06 08:38:03 -0300

    Update questao3.c

--- questao3.c
+++ questao3.c
@@ -5,18 +5,16 @@ int main() {
     int vetorA[16];
     int vetorB[16];
     int matriz[4][4];
-    char operacao[10]; 
+    char operacao[10];
 
     for (int i = 0; i < 16; i++) {
         scanf("%d", &vetorA[i]);
     }
 
-  
     for (int i = 0; i < 16; i++) {
         scanf("%d", &vetorB[i]);
     }
 
-  
     scanf("%s", operacao);
 
     if (strcmp(operacao, "soma") == 0) {
@@ -28,13 +26,11 @@ int main() {
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                printf("%d ", matriz[i][j]);
+                printf("%4d", matriz[i][j]);
             }
             printf("\n");
         }
-    } 
-
-    else if (strcmp(operacao, "sub") == 0) {
+    } else if (strcmp(operacao, "sub") == 0) {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 matriz[i][j] = vetorA[i * 4 + j] - vetorB[i * 4 + j];
@@ -43,31 +39,27 @@ int main() {
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                printf("%d ", matriz[i][j]);
+                printf("%4d", matriz[i][j]);
+            }
+            printf("\n");
+        }
+    } else if (strcmp(operacao, "mult") == 0) {
+        for (int i = 0; i < 4; i++) {
+          for (int j = 0; j < 4; j++) {
+            matriz[i][j] = 0;
+               for (int k = 0; k < 4; k++) {
+                     matriz[i][j] += vetorA[i * 4 + k] * vetorB[k * 4 + j];
+               }
+            }
+         }
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                printf("%4d", matriz[i][j]);
             }
             printf("\n");
         }
     }
-
-    else if (strcmp(operacao, "mult") == 0) {
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                matriz[i][j] = 0;
-                for (int k = 0; k < 4; k++) {
-                    matriz[i][j] += vetorA[i * 4 + k] * vetorB[k * 4 + j];
-                }
-            }
-        }
-
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                printf("%d ", matriz[i][j]);
-            }
-            printf("\n");
-        }
-    }
-
-
 
     return 0;
 }
