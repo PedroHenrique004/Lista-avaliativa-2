588e9b121762f8347676b69fcabbe2a09de0c02a
Pedro Santos <124071877+PedroHenrique004@users.noreply.github.com>
2023-11-07 11:38:19 -0300

    Update questao3.c

--- questao3.c
+++ questao3.c
@@ -1,64 +1,67 @@
 #include <stdio.h>
+#include <locale.h>
 #include <string.h>
 
+#define tamanho 4
+
+void lerMatriz(int matriz[tamanho][tamanho]) {
+    for (int i = 0; i < tamanho; i++)
+        for (int j = 0; j < tamanho; j++)
+            scanf("%d", &matriz[i][j]);
+}
+
+void somar(int m1[tamanho][tamanho], int m2[tamanho][tamanho], int resultado[tamanho][tamanho]) {
+    for (int i = 0; i < tamanho; i++)
+        for (int j = 0; j < tamanho; j++)
+            resultado[i][j] = m1[i][j] + m2[i][j];
+}
+
+void subtrair(int m1[tamanho][tamanho], int m2[tamanho][tamanho], int resultado[tamanho][tamanho]) {
+    for (int i = 0; i < tamanho; i++)
+        for (int j = 0; j < tamanho; j++)
+            resultado[i][j] = m1[i][j] - m2[i][j];
+}
+
+void multiplicar(int m1[tamanho][tamanho], int m2[tamanho][tamanho], int resultado[tamanho][tamanho]) {
+    for (int i = 0; i < tamanho; i++) {
+        for (int j = 0; j < tamanho; j++) {
+            resultado[i][j] = 0;
+            for (int k = 0; k < tamanho; k++)
+                resultado[i][j] += m1[i][k] * m2[k][j];
+        }
+    }
+}
+
+void realizarOperacao(int matriz1[tamanho][tamanho], int matriz2[tamanho][tamanho], char operacao[5], int resultado[tamanho][tamanho]) {
+    if (strcmp(operacao, "soma") == 0) {
+        somar(matriz1, matriz2, resultado);
+    } else if (strcmp(operacao, "sub") == 0) {
+        subtrair(matriz1, matriz2, resultado);
+    } else if (strcmp(operacao, "mult") == 0) {
+        multiplicar(matriz1, matriz2, resultado);
+    } else {
+        printf("\nOperação desconhecida.\n");
+    }
+}
+
 int main() {
-    int vetorA[16];
-    int vetorB[16];
-    int matriz[4][4];
-    char operacao[10];
+    int matriz1[tamanho][tamanho]; 
+    int matriz2[tamanho][tamanho]; 
+    int resultado[tamanho][tamanho]; 
+    char operacao[5]; 
 
-    for (int i = 0; i < 16; i++) {
-        scanf("%d", &vetorA[i]);
-    }
-
-    for (int i = 0; i < 16; i++) {
-        scanf("%d", &vetorB[i]);
-    }
-
+    setlocale(LC_ALL, "Portuguese");
+    lerMatriz(matriz1);
+    lerMatriz(matriz2);
     scanf("%s", operacao);
 
-    if (strcmp(operacao, "soma") == 0) {
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                matriz[i][j] = vetorA[i] + vetorB[j];
-            }
-        }
+    realizarOperacao(matriz1, matriz2, operacao, resultado);
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                printf("%4d", matriz[i][j]);
-            }
-            printf("\n");
+    for (int i = 0; i < tamanho; i++) {
+        for (int j = 0; j < tamanho; j++) {
+            printf("%4d", resultado[i][j]);
         }
-    } else if (strcmp(operacao, "sub") == 0) {
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                matriz[i][j] = vetorA[i * 4 + j] - vetorB[i * 4 + j];
-            }
-        }
-
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                printf("%4d", matriz[i][j]);
-            }
-            printf("\n");
-        }
-    } else if (strcmp(operacao, "mult") == 0) {
-        for (int i = 0; i < 4; i++) {
-          for (int j = 0; j < 4; j++) {
-            matriz[i][j] = 0;
-               for (int k = 0; k < 4; k++) {
-                     matriz[i][j] += vetorA[i * 4 + k] * vetorB[k * 4 + j];
-               }
-            }
-         }
-
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                printf("%4d", matriz[i][j]);
-            }
-            printf("\n");
-        }
+        printf("\n");
     }
 
     return 0;
